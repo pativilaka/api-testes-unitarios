c9ce4edd72d658336e0eb6ab4568be43b435e66a
pativilaka <p.vilaka@gmail.com>
2023-09-18 12:07:13 -0300

    Criação Interface Service

---
+++ src/main/java/com/vilaka/api/testes/unitarios/apitestesunitarios/services/UserService.java
@@ -0,0 +1,8 @@
+package com.vilaka.api.testes.unitarios.apitestesunitarios.services;
+
+import com.vilaka.api.testes.unitarios.apitestesunitarios.domain.User;
+
+public interface UserService {
+
+    User findById(Integer id);
+}
