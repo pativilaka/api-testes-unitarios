96add7fb04e8ecd556f886b10a77ed7eb39bbe43
pativilaka <p.vilaka@gmail.com>
2023-09-18 10:20:16 -0300

    first commit

---
+++ src/main/java/com/vilaka/api/testes/unitarios/apitestesunitarios/ApiTestesUnitariosApplication.java
@@ -0,0 +1,13 @@
+package com.vilaka.api.testes.unitarios.apitestesunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestesUnitariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilaka/api/testes/unitarios/apitestesunitarios/ApiTestesUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilaka.api.testes.unitarios.apitestesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesUnitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
